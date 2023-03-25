81db5418f9a7868bc63452156e503a88029a3678
David Vashakidze <83545222+vashaka@users.noreply.github.com>
2023-03-25 19:37:07 +0400

    checked

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -4,6 +4,8 @@ import Layout from "../components/Layout";
 import { AppContextProvider } from "@/context/appContext";
 import axios from "axios";
 
+// THIS SHOULD BE CHANGED
+
 axios.defaults.baseURL = "http://localhost:4001";
 
 export default function App({ Component, pageProps }: AppProps) {
